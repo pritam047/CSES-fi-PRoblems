0f4d3d539dbe5dd0e7917f181d6aa1cf95ab0c5a
PritamKar <pritamkar13@gmail.com>
2020-10-03 18:58:43 +0530

    Revert "Solutions are added"

--- JithinAntony4/JithinAntony4_weird_algorithm.py
+++
@@ -1,9 +0,0 @@
-# Weird Algorithm
-# cses.fi/1068
-N=int(input());print(N)
-while N!=1:
-    if N%2==0:N//=2
-    else:N=N*3+1
-    print(N)
-# author: Jithin Antony
-# profile: https://cses.fi/user/32631
